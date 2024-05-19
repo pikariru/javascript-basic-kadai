ae009dd41c285d6996eeb44aec8b14e2b21de790
pikariru <167439420+pikariru@users.noreply.github.com>
2024-05-19 10:59:30 +0900

    Add files via upload

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+var currentDate = new Date();
+
+var year = currentDate.getFullYear();
+var month = currentDate.getMonth() + 1; 
+var day = currentDate.getDate();
+
+console.log(year + "年" + month + "月" + day + "日");
