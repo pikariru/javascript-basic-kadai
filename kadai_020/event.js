2242e726b1ea47e7d18611a00f3098fc57c89b56
pikariru <167439420+pikariru@users.noreply.github.com>
2024-05-22 22:16:09 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("btn");
+
+button.addEventListener("click", function() {   
+    var displayText = document.getElementById("text");
+    displayText.textContent = "ボタンをクリックしました";
+});
